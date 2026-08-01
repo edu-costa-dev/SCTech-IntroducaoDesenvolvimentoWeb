5c1307bfbf2fb262acede9af03840c26dab3e641
Antonio Eduardo <antonio_eduardo96@icloud.com>
2026-08-01 17:40:21 -0300

    subindo aula 20

---
+++ Aula20/script.js
@@ -0,0 +1,9 @@
+const frutas = ["maçã", "uva", "laranja", "maracuja", "abacaxi"];
+console.log(frutas);
+
+console.log(frutas[2]); // Acessando o elemento na posição 2 do array 3º item da atividade.
+frutas[1] = "Kiwi"; // Alterando o segundo item do array para "Kiwi"
+console.log(frutas); //Mostrando o array atualizado com a alteração do segundo item.
+frutas.push("uva"); // Adicionando um novo elemento ao final do array
+frutas.push("limão"); // Adicionando um novo elemento ao final do array
+console.log(frutas);// Mostrando o array atualizado com os novos elementos adicionados ao final.
