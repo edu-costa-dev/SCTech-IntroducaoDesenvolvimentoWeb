df37cd6c841d12ab7aaa94258a5865d3764a9939
Antonio Eduardo <95864631+edu-costa-dev@users.noreply.github.com>
2026-08-05 20:20:44 +0000

    Subindo aula

--- Back-End_Aula08/app_expressjs/app.ts
+++ Back-End_Aula08/app_expressjs/app.ts
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("<h1>Rota Inicial</h1>")
+})
+
+app.get('/sobrenos', (req, res) => {
+    res.send("Rota falando sobre a empresa")
+})
+
+app.get('/trabalheConosco', (req, res) => {
+    res.send("ROpções de Carreira")
+})
+
+app.get('/Contato', (req, res) => {
+    res.send("48-9999-9999")
+})
+
 app.listen(3000, () => {
     console.log("Servidor criado...")
 })
